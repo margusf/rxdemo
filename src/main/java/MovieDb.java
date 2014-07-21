ba58eb74201588e8e19e5d9af33e7981cf282bc5
Margus Freudenthal <margus@cyber.ee>
2014-07-21 13:13:00 +0300

    Calls to movie database.

--- src/main/java/MovieDb.java
+++ src/main/java/MovieDb.java
@@ -1,11 +1,69 @@
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Connection to movie database:
  * http://docs.themoviedb.apiary.io/
  */
-class MovieDb {
-    static void getMovies() throws Exception {
+public class MovieDb {
+    private static final Gson GSON = new Gson();
+
+    static MovieInfo[] getMovies() throws Exception {
         // GEThttp://api.themoviedb.org/3/discover/movie
-        System.out.println(
-                HttpHelper.doGet("http://api.themoviedb.org/3/discover/movie?api_key=0835385142505861236af08685de3c1f"));
+        String json = HttpHelper.doGet(
+                "http://api.themoviedb.org/3/discover/movie?api_key=0835385142505861236af08685de3c1f");
+
+        DiscoverResponse response = GSON.fromJson(
+                json, DiscoverResponse.class);
+        return response.getResults();
+    }
+
+    static ActorInfo[] getCast(MovieInfo movie) throws Exception {
+        String json = HttpHelper.doGet(
+                        "http://api.themoviedb.org/3/movie/" + movie.getId()
+                                + "/credits?api_key=0835385142505861236af08685de3c1f");
+        CastResponse response = GSON.fromJson(json, CastResponse.class);
+        return response.getCast();
+    }
+
+    static ActorDetails getActorDetails(ActorInfo actor) throws Exception {
+        String json = HttpHelper.doGet(
+                        "http://api.themoviedb.org/3/person/" + actor.getId()
+                                + "?api_key=0835385142505861236af08685de3c1f");
+        ActorDetails response = GSON.fromJson(json, ActorDetails.class);
+        return response;
+    }
+
+    @Data
+    static class MovieInfo {
+        private int id;
+        private String title;
+        @SerializedName("release_date")
+        private String releaseDate;
+    }
+
+    @Data
+    static class DiscoverResponse {
+        private MovieInfo[] results;
+    }
+
+    @Data
+    static class CastResponse {
+        private ActorInfo[] cast;
+    }
+
+    @Data
+    static class ActorInfo {
+        private int id;
+        private String name;
+    }
+
+    @ToString(callSuper=true)
+    static class ActorDetails extends ActorInfo {
+        @Getter @Setter private String birthday;
     }
 }
--- src/test/java/MovieDBTest.java
+++ src/test/java/MovieDBTest.java
@@ -1,8 +1,27 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class MovieDBTest {
     @Test
     public void testGetMovies() throws Exception {
-        MovieDb.getMovies();
+        MovieDb.MovieInfo[] movies = MovieDb.getMovies();
+        System.out.println(Arrays.toString(movies));
+    }
+
+    @Test
+    public void testGetCast() throws Exception {
+        MovieDb.MovieInfo cowgirlBlues = new MovieDb.MovieInfo();
+        cowgirlBlues.setId(264688);
+        MovieDb.ActorInfo[] actors = MovieDb.getCast(cowgirlBlues);
+        System.out.println(Arrays.toString(actors));
+    }
+
+    @Test
+    public void testGetActorDetails() throws Exception {
+        MovieDb.ActorInfo drakeHogestyn = new MovieDb.ActorInfo();
+        drakeHogestyn.setId(9274);
+        MovieDb.ActorDetails details = MovieDb.getActorDetails(drakeHogestyn);
+        System.out.println(details);
     }
 }
